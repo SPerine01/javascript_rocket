f76501f5a349730426d1405fc5b1fcd9b789addd
Sumayyah Perine <sperine01@gmail.com>
2015-12-24 18:20:15 -0500

    edits to javascript and css, rocket launching

--- js/rocket.js
+++ js/rocket.js
@@ -4,16 +4,31 @@ var countdownNumber = 5;
 var changeState = function(state) {
 	document.body.className = 'body-state' + state;
 	clearInterval(timer);
+	countdownNumber = 5;
+	document.getElementById('countdown').innerHTML = countdownNumber;
 
+// countdown
 	if (state == 2) {
 		timer = setInterval(function () {
-			document.getElementById('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber -1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
 			if (countdownNumber <= 0) {
 				changeState(3);
 			}
 		}, 1000);
 	} else if (state == 3) {
 
+		// success
+		var success = setTimeout(function()
+			{
+				var randomNumber = Math.round(Math.random()*10);
+				console.log("randomNumber:", randomNumber)
+
+				if (randomNumber > 2) {
+					changeState(4);
+				} else {
+					changeState(5); 
+				}
+			}, 2000);
 	}
 };
