cc73f88bbb58a2667cdd888d71e5665aa169af60
Sumayyah Perine <sperine01@gmail.com>
2015-12-24 23:35:48 -0500

    add more js and css for rocket functionality

--- js/rocket.js
+++ js/rocket.js
@@ -12,22 +12,30 @@ var changeState = function(state) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber -1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				// yay for happiness
+				document.getElementById("yay").className = "yay show";
+			} else {
+				document.getElementById("yay").className = "yay";
+			}
+
 			if (countdownNumber <= 0) {
 				changeState(3);
 			}
 		}, 1000);
 	} else if (state == 3) {
 
-		// success
+// success
 		var success = setTimeout(function()
 			{
 				var randomNumber = Math.round(Math.random()*10);
-				console.log("randomNumber:", randomNumber)
+				console.log("randomNumber:", randomNumber);
 
-				if (randomNumber > 2) {
+				if (randomNumber > 4) {
 					changeState(4);
 				} else {
-					changeState(5); 
+					changeState(5);
 				}
 			}, 2000);
 	}
